f0a78a6bafcf9f464e20da33dc0ed3a61bb60ab5
Kerem VARIŞ <keremvaris@hotmail.com.tr>
2021-01-15 23:19:57 +0300

    Minor Changes

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -26,8 +26,9 @@ module.exports = {
         },       
         {
           href: 'https://github.com/DevArchitecture',
-          label: 'DevArchitecture GitHub',
           position: 'right',
+          className: 'header-github-link',
+          'aria-label': 'DevArchitecture GitHub',
         },
       ],
     },
