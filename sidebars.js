d866790688fa4b47766f731b2e33700863447cd5
Kerem VARIŞ <keremvaris@hotmail.com.tr>
2021-04-21 13:25:06 +0300

    Proje yayınlama WebApi eklendi.

--- sidebars.js
+++ sidebars.js
@@ -2,7 +2,7 @@ module.exports = {
   someSidebar: {
     DevArchitecture : ['baslarken','kurulum','veritabani','ortamkurulumu','yeniproje','veritabanisecimi','migration',
     'projecalistirmawebapi','projecalistirmaangular','projekatmanlari'
-    ,'projegelistirmeyaklasimlari','testrutinleri'], //,'projeyayinlama'
+    ,'projegelistirmeyaklasimlari','testrutinleri','projeyayinlama'], //,'projeyayinlama'
     Nasıl_Yapılır: ['yenimodulrdbms','yenicomponentangular'] ,
     Gelişmiş:['cache','log','transaction','validation','multilanguage','elasticsearch','rabbitmq','entegrasyon'],
     //CodeGenerator:['codegen'],
